07a6e0362532d766e984a49c21f49ccbeb1a5414
Rahulkumar Das <48314756+rahulkumar-das@users.noreply.github.com>
2020-01-28 10:26:40 +0530

    Create main2.java

---
+++ main2.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public static void main(String args[])
+{
+System.out.println("Devops 2nd Experiment");
+  System.out.print("This is a test file");
+  //This is comment
+  Scanner sc = new Scanner(System.in)
+}
